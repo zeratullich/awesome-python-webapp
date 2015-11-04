7470e2a6dca9370c41c0cbc4725455523dac3391
zeratullich <lichuanchou@163.com>
2015-11-04 15:39:12 +0800

    day 4...

--- www/transwarp/test6.py
+++ www/transwarp/test6.py
@@ -10,12 +10,14 @@ from time import ctime,sleep
 def music(func):
     for i in range(2):
         print "I was listening to music %s. %s" %(func,ctime())
-        sleep(1)
+        sleep(6)
+        print 'the music "%s" has end at %s' % (func,ctime())
 
 def move(func):
     for i in range(2):
         print "I was at the movies %s! %s" %(func,ctime())
-        sleep(5)
+        sleep(4)
+        # print 'the movie "%s" has end at %s!' %(func,ctime())
 
 class MyThread(threading.Thread):
     def __init__(self,*arg,**kw):
@@ -24,24 +26,26 @@ class MyThread(threading.Thread):
 
 threads=[]
 t1=MyThread(target=music,args=('K歌之王',))
+t1.setName('线程1')
 threads.append(t1)
 t2=MyThread(target=move,args=('黑暗骑士',))
+t2.setName('线程2')
 threads.append(t2)
 print threads
 if __name__ == '__main__':
     # music('K歌之王')
     # move('黑暗骑士')
     print 'the main process is %s' % (threading.current_thread().name)
-    # for t in threads:
-    #     # t.setDaemon(True)
-    #     t.start()
-    #     # print t
-    # t.join()
-    t1.start()
-    t2.start()
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+        # print t
+    t.join()
+    # t1.start()
+    # t2.start()
     # t1.join()
-    print t2
-    t2.join()
+    print t.name
+    # t2.join()
 
     print "all over %s . the main process is %s" % (ctime(),threading.current_thread().name)
 
